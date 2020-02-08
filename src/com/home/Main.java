9e0e6462d891c6d9e4abc5d1bfbb44d2d11ebd88
nemezis <nemezisresident1985@gmail.com>
2020-02-08 18:31:21 +0300

    ver 1

---
+++ src/com/home/Main.java
@@ -0,0 +1,12 @@
+package com.home;
+
+/**
+ * Created by Nemezis on 08.02.2020.
+ */
+public class Main {
+
+    public static void main (String [] args){
+
+        System.out.println("Hellow world");
+    }
+}
